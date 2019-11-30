5a0addd2e0c4fe53e0eda1e48f74fb8c447f49fc
Juan Pablo Pereira Ribera <juanpablopr_@hotmail.com>
2019-11-30 13:34:51 -0400

    pruebas finalizadas

---
+++ Pereira/Juan/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace Juan
+{
+    public class Company
+    {
+        private string _name;
+        private double _saveproject;
+
+        public string Name { get { return _name; } }
+        public double SaveProject { get { return _saveproject; } }
+
+        public Company(string name, double saveproyect)
+        {
+            _name = name;
+            _saveproject = saveproyect;
+
+        }
+        public void Save(double save)
+        {
+            _saveproject += save;
+
+        }
+        public void Spend(double save)
+        {
+            _saveproject -= save;
+        }
+        public void ChangeName(string name)
+        {
+            _name = name;
+        }
+    }
+
+}
---
+++ Pereira/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Juan;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Prueba_Save()
+        {
+            //organizar
+            Company comp = new Company("frio", 25.36);
+            double ValorEsperado = 35.36;
+
+
+
+            //actuar
+            comp.Save(10);
+            double ValorObtenido = comp.SaveProject;
+
+
+            //afirmar
+            Assert.AreEqual(ValorEsperado, ValorObtenido);
+        }
+
+        [TestMethod]
+        public void Prueba_Spend()
+        {
+            //organizar
+            Company comp = new Company("frio", 25.36);
+            double ValorEsperado = 15.36;
+
+
+
+            //actuar
+            comp.Spend(10);
+            double ValorObtenido = comp.SaveProject;
+
+
+            //afirmar
+            Assert.AreEqual(ValorEsperado, ValorObtenido);
+        }
+        [TestMethod]
+        public void Prueba_Nombre()
+        {
+            //organizar
+            Company comp = new Company("frio", 25.36);
+            string ValorEsperado = "juan";
+
+
+
+            //actuar
+            comp.ChangeName("juan");
+            string ValorObtenido = comp.Name;
+
+
+            //afirmar
+            Assert.AreEqual(ValorEsperado, ValorObtenido);
+        }
+
+    }
+}
